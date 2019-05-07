05129a72c31b843d91df42ddaea6a935773db81e
heavenrock03 <50034363+heavenrock03@users.noreply.github.com>
2019-05-07 12:21:13 +0200

    Add files via upload

--- figuras/rectangulo.py
+++ figuras/rectangulo.py
@@ -3,7 +3,7 @@ from coordenada2 import *
 class rectangulo():
     
     def __init__(self):
-        self.p1 = coordenada() #p1, va a tener los atributos de coordenada igual que p2#
+        self.p1 = coordenada() #p1, va a tener los atributos de coordenada igual que p2, que son X y Y#
         self.p2 = coordenada()
         self.area = 0
 
@@ -13,6 +13,6 @@ class rectangulo():
 
     def calculararea(self):
         p3 = coordenada()
-        p3.x = self.p1.x
-        p3.y = self.p2.y
-        self.area = p3.distancia(self.p1) * p3.distancia(self.p2)
+        p3.x = self.p1.x #x de p3 va a ser igual al x de p1#
+        p3.y = self.p2.y #y de p3 va a ser igual al y de p2# 
+        self.area = p3.distancia(self.p1) * p3.distancia(self.p2)#el area es el producto entre las distancias entre (p3 y p1) y (p3 y p2)#
