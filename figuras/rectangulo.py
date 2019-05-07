480c5b3f2c383aaac98d9df953765502044928f3
heavenrock03 <50034363+heavenrock03@users.noreply.github.com>
2019-05-07 11:57:44 +0200

    Update rectangulo.py

--- figuras/rectangulo.py
+++ figuras/rectangulo.py
@@ -3,7 +3,7 @@ from coordenada2 import *
 class rectangulo():
     
     def __init__(self):
-        self.p1 = coordenada()
+        self.p1 = coordenada() #p1, va a tener los atributos de coordenada igual que p2#
         self.p2 = coordenada()
         self.area = 0
 
